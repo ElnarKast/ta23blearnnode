b5b044567cd0d1ca38a75e66e57c4d1fe9ab381b
Elnar Kast <elnar.kast@tptlive.ee>
2024-11-09 18:25:41 +0200

    Add html-webpack-plugin

--- learnnode/webpack.config.js
+++ learnnode/webpack.config.js
@@ -1,11 +1,16 @@
 import path from "path";
+import HtmlWebpackPlugin from 'html-webpack-plugin'
 
 const __filename = import.meta.filename;
 const __dirname = import.meta.dirname;
+
 export default {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin(),
+    ],
 }
