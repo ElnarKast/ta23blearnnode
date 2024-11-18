8a72297631da76a95a18137bd89626c07eb1a754
Elnar Kast <elnar.kast@tptlive.ee>
2024-11-18 11:54:00 +0200

    1811bubas

--- learnnode/webpack.config.js
+++ learnnode/webpack.config.js
@@ -1,62 +1,85 @@
 import path from "path";
-import HtmlWebpackPlugin from 'html-webpack-plugin'
-import loader from "sass-loader";
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
 
 const __filename = import.meta.filename;
 const __dirname = import.meta.dirname;
 
-export default {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'public'),
+let name = 'Elnar Kast';
+
+export default async () => {
+    const response = await fetch('https://rickandmortyapi.com/api/character')
+    const data = await response.json();
+    const characters = data.results;
+    const pages = [];
+    characters.forEach(character => {
+        let page = new HtmlWebpackPlugin({
+            template: "./src/about.njk",
+            filename: `character_${character.id}.html`,
+            templateParameters: {
+                character,
+            }
+        });
+        pages.push(page)
+    });
+    return {
+        entry: './src/index.js',
+        output: {
+            filename: 'main.js',
+            path: path.resolve(__dirname, 'dist')
         },
-        compress: true,
-        port: 9000,
-    },
-    module: {
-        rules: [
-            {
-               test: /\.css$/i,
-               use: ["style-loader", "css-loader"],
+        devServer: {
+            static: {
+                directory: path.join(__dirname, 'public'),
             },
-            {
-                test: /\.scss$/i,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sassOptions: {
-                                quietDeps: true,
+            compress: true,
+            port: 9000,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: ["style-loader", "css-loader"],
+                },
+                {
+                    test: /\.scss$/i,
+                    use: [
+                        "style-loader",
+                        "css-loader",
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sassOptions: {
+                                    quietDeps: true,
+                                }
                             }
                         }
-                    }
-                ],
-             },
-             {
-                test: /\.njk$/,
-                use: [
-                    {
-                        loader: 'simple-nunjucks-loader',
-                        options: {}
-                    }
-                ]
-             }
+                    ],
+                },
+                {
+                    test: /\.njk$/,
+                    use: [
+                        {
+                            loader: 'simple-nunjucks-loader',
+                            options: {}
+                        }
+                    ]
+                }
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/index.njk',
+                templateParameters: {
+                    name,
+                    characters,
+                }
+            }),
+            new HtmlWebpackPlugin({
+                template: './src/about.njk',
+                filename: 'about.html'
+            }),
+            ...pages
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.njk',
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/about.njk',
-            filename: 'about.html'
-        }),
-    ],
+    };
 };
