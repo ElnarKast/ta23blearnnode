7ff2a76c58ab9bbf633df6d2a3cba050a35fe4ed
Elnar Kast <elnar.kast@tptlive.ee>
2024-12-02 11:41:08 +0200

    vue

---
+++ learnnode/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+let message = 'Hello Vue!'
+</script>
+<template>
+    <div class="container">
+        <div class="content"><h1>{{ message }}</h1>
+        </div>
+    </div>
+<h1>{{ message }}</h1>
+</template>
+<style>
+
+</style>
--- learnnode/webpack.config.js
+++ learnnode/webpack.config.js
@@ -1,85 +1,60 @@
 import path from "path";
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-
+import { VueLoaderPlugin } from "vue-loader";
+import webpack from 'webpack';
 
 const __filename = import.meta.filename;
 const __dirname = import.meta.dirname;
 
-let name = 'Elnar Kast';
-
-export default async () => {
-    const response = await fetch('https://rickandmortyapi.com/api/character?page=13')
-    const data = await response.json();
-    const characters = data.results;
-    const pages = [];
-    characters.forEach(character => {
-        let page = new HtmlWebpackPlugin({
-            template: "./src/character.njk",
-            filename: `character_${character.id}.html`,
-            templateParameters: {
-                character,
-            }
-        });
-        pages.push(page)
-    });
-    return {
-        entry: './src/index.js',
-        output: {
-            filename: 'main.js',
-            path: path.resolve(__dirname, 'dist')
+export default {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
         },
-        devServer: {
-            static: {
-                directory: path.join(__dirname, 'public'),
+        compress: true,
+        port: 9000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
             },
-            compress: true,
-            port: 9000,
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.css$/i,
-                    use: ["style-loader", "css-loader"],
-                },
-                {
-                    test: /\.scss$/i,
-                    use: [
-                        "style-loader",
-                        "css-loader",
-                        {
-                            loader: 'sass-loader',
-                            options: {
-                                sassOptions: {
-                                    quietDeps: true,
-                                }
+            {
+                test: /\.scss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sassOptions: {
+                                quietDeps: true,
                             }
                         }
-                    ],
-                },
-                {
-                    test: /\.njk$/,
-                    use: [
-                        {
-                            loader: 'simple-nunjucks-loader',
-                            options: {}
-                        }
-                    ]
-                }
-            ],
-        },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: './src/index.njk',
-                templateParameters: {
-                    name,
-                    characters,
-                }
-            }),
-            new HtmlWebpackPlugin({
-                template: './src/about.njk',
-                filename: 'about.html'
-            }),
-            ...pages
+                    }
+                ],
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
         ],
-    };
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new VueLoaderPlugin(),
+        new webpack.DefinePlugin({
+            __VUE_OPTIONS_API__: 'true',
+            __VUE_PROD_DEVTOOLS__: 'false',
+            __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
+        })
+    ],
 };
