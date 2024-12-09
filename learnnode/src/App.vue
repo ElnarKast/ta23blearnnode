d22151d804622392d2ffff9fd5edf836108a336a
Elnar Kast <elnar.kast@tptlive.ee>
2024-12-09 11:31:42 +0200

    09.12

--- learnnode/src/App.vue
+++ learnnode/src/App.vue
@@ -1,13 +1,33 @@
 <script setup>
-let message = 'Hello Vue!'
+import { ref } from 'vue';
+
+let items = ref(['Milk', 'Bread', 'Vodka', 'Beer', 'Chips']);
+let newItem = ref('');
+
+function add(){
+    if(newItem.value.trim() !== ''){
+    items.value.push(newItem.value)
+    }
+    newItem.value="";
+}
+
 </script>
 <template>
-    <div class="container">
-        <div class="content"><h1>{{ message }}</h1>
+    <div class="container mt-3">
+        <div class="content">
+            <div class="fields has-addons">
+                <div class="controll is-expanded">
+                    <input class="input" type = "text" v-model="newItem" @keypress.enter = "add">
+                </div>
+                <div class="control">
+                    <button class="button is-primary" @click="add">Add</button>
+                </div>
+            </div>    
+
+            <ul>
+                <li v-for="item in items">{{ item }}</li>
+            </ul>
         </div>
     </div>
-<h1>{{ message }}</h1>
-</template>
-<style>
-
-</style>
+</template>,
+<style></style>
