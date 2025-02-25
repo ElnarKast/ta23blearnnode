b1934b4ffaad83d19b2644b40aed0351e84d2f62
Elnar <elnar.kast@tptlive.ee>
2025-02-25 14:02:29 +0200

    index i webpack ne dodelal

--- learnnode/src/App.vue
+++ learnnode/src/App.vue
@@ -1,24 +1,8 @@
 <script setup>
 import { ref } from 'vue';
-import Tabs from './components/Tabs.vue';
-import ToDo from './pages/ToDo.vue';
-import Modals from './pages/Modals.vue';
-
-let titles = [
-    'ToDo',
-    'Modals',
-];
-       
-let contents = [
-    ToDo,
-    Modals,
-]
-let activeTab = ref(0);
-function change(key){
-    activeTab.value = key;
-}
+import RouterTabs from './components/RouterTabs.vue';
 </script>
 <template>
-    <Tabs :items="titles" @change="change"></Tabs>
-    <component :is="contents[activeTab]"></component>
+<RouterTabs></RouterTabs>
+<RouterView></RouterView>
 </template>
