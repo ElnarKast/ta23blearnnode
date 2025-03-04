4204e1d65158989a487b37f53010ced7724f0556
Elnar <elnar.kast@tptlive.ee>
2025-03-04 13:34:11 +0200

    Add ChuckNorris

--- learnnode/src/App.vue
+++ learnnode/src/App.vue
@@ -3,6 +3,10 @@ import { ref } from 'vue';
 import RouterTabs from './components/RouterTabs.vue';
 </script>
 <template>
-<RouterTabs></RouterTabs>
-<RouterView></RouterView>
+    <RouterTabs></RouterTabs>
+    <div class="container">
+        <section class="section">
+            <RouterView></RouterView>
+        </section>
+    </div>
 </template>
