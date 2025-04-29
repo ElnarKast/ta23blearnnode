4742e253989487b0dad57d4d4981f6e0900835e6
ElnarKast <elnar.kast@tptlive.ee>
2025-04-29 11:34:01 +0300

    Leaflet map

---
+++ learnnode/src/pages/Leaflet.vue
@@ -0,0 +1,13 @@
+<script setup>
+ import { ref } from 'vue';
+ import LeafletMap from '../components/LeafletMap.vue';
+ 
+ let coords = ref([59.42691, 24.74344]);
+ let zoom = ref(19);
+ </script>
+ <template>
+     <button class="button is-primary" @click="coords=[58.378003935892494, 26.727245723399832]">Go to Tartu</button>
+     <input type="range" min="1" max="19" step="1" v-model="zoom">
+     <LeafletMap :center="coords" :zoom="zoom"></LeafletMap>
+     <LeafletMap :center="[59.4398836,24.7532123,17]" :zoom="10"></LeafletMap>
+ </template>
